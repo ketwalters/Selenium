d824450c7bf23bc5a199c70df5911fab4f25c4b1
ketwalters <ketwalters@gmail.com>
2018-01-12 13:02:08 -0500

    add checkout form method

--- online-store_automation.rb
+++ online-store_automation.rb
@@ -22,6 +22,7 @@ class Start < Base
   def begin
     setup
     choose_and_check
+    checkout_form
     teardown
   end
 
@@ -30,6 +31,18 @@ class Start < Base
     product_page('Apple iPad 2 16GB, Wi-Fi, 9.7in – Black')
     add_to_cart
     checkout
+  end
+
+  def checkout_form
+    @driver.find_element(:id, "wpsc_checkout_form_9").send_keys("test123@test.com")
+    @driver.find_element(:id, "wpsc_checkout_form_2").send_keys("Kay")
+    @driver.find_element(:id, "wpsc_checkout_form_3").send_keys("Clarke")
+    @driver.find_element(:id, "wpsc_checkout_form_4").send_keys("1203 Main Street")
+    @driver.find_element(:id, "wpsc_checkout_form_5").send_keys("Brooklyn")
+    @driver.find_element(:id, "uniform-wpsc_checkout_form_7").find_element(:css,"option[value='US']").click
+    @driver.find_element(:id, "wpsc_checkout_form_8").send_keys("10001")
+    @driver.find_element(:id, "wpsc_checkout_form_18").send_keys("123-345-6789")
+    @driver.find_element(:class, "input-button-buy").click
   end
 
   def product_category(product)
@@ -51,6 +64,7 @@ class Start < Base
     @driver.find_element(:class, "cart_icon").click
     wait
     @driver.find_element(:class, "step2").click
+    wait
   end
 
 end
