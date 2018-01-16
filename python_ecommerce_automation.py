f9e8ee16bef830b870fbc47aa3340acb39cd9290
ketwalters <ketwalters@gmail.com>
2018-01-15 21:05:21 -0500

    test hover and click, add timeout

--- python_ecommerce_automation.py
+++ python_ecommerce_automation.py
@@ -2,13 +2,14 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 
 class MyStoreTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-
+    
     def test_title(self):
         self.browser.get('http://automationpractice.com/index.php')
         header = self.browser.find_element(By.ID, 'header_logo')
@@ -21,7 +22,7 @@ class MyStoreTest(unittest.TestCase):
         elem.send_keys("dress")
         self.browser.find_element_by_name('submit_search').click()
 
-    def test_hover(self):
+    def test_hover_and_category_click(self):
         driver = self.browser
         driver.get('http://automationpractice.com/index.php')
         element_to_hover_over = driver.find_element_by_xpath(".//a[contains(text(), 'Women')]")
@@ -29,6 +30,9 @@ class MyStoreTest(unittest.TestCase):
         hover = ActionChains(driver).move_to_element(element_to_hover_over)
         hover.perform()
 
+        ele_to_click = driver.find_element_by_xpath(".//a[contains(text(), 'T-shirts')]")
+        time.sleep(10)
+        ele_to_click.click()
 
     def tearDown(self):
         self.browser.close()
